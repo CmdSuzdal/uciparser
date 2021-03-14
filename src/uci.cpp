15741e495f84f771854f40dbff79a7f9ef7f4fbe
Francesco Piantini <francesco.piantini@gmail.com>
2021-03-14 12:53:21 +0100

    parse() method: added management of character case and leading and trailing spaces

--- src/uci.cpp
+++ src/uci.cpp
@@ -1,13 +1,44 @@
+#include<bits/stdc++.h>
+#include <algorithm>
+
 #include "uciparser/uci.h"
+
+
 
 namespace UciParser
 {
 
+    // ----------------------------------------------------------------
+    // Utility functions
+    // courtesy of:
+    // https://www.techiedelight.com/trim-string-cpp-remove-leading-trailing-spaces/
+    const std::string WHITESPACE = " \n\r\t\f\v";
+
+    std::string ltrim(const std::string& s)
+    {
+        size_t start = s.find_first_not_of(WHITESPACE);
+        return (start == std::string::npos) ? "" : s.substr(start);
+    }
+    std::string rtrim(const std::string& s)
+    {
+        size_t end = s.find_last_not_of(WHITESPACE);
+        return (end == std::string::npos) ? "" : s.substr(0, end + 1);
+    }
+    std::string trim(const std::string& s)
+    {
+        return rtrim(ltrim(s));
+    }
+    // ----------------------------------------------------------------
+
+    // ----------------------------------------------------------------
+    // UciParser class implementation
     unsigned int UciParser::parse(std::string str)
     {
-        if (str == "uci")
+        transform(str.begin(), str.end(), str.begin(), ::tolower);
+        if (trim(str) == "uci")
             cmd = UCICMD_UCI;
         return cmd;
     }
+    // ----------------------------------------------------------------
 
 }   // namespace UciParser
--- test/uciinittest.cpp
+++ test/uciinittest.cpp
@@ -28,5 +28,28 @@ namespace UciParser
         ASSERT_EQ(up.parse("uci"), UCICMD_UCI);
         ASSERT_EQ(up.cmd, UCICMD_UCI);
     }
+    TEST_F(AnUciParser, Return_UCI_CommandWhenParsesAStringEqualTo_uci_AlsoWithCaseVariations)
+    {
+        ASSERT_EQ(up.parse("Uci"), UCICMD_UCI);
+        ASSERT_EQ(up.cmd, UCICMD_UCI);
+        ASSERT_EQ(up.parse("uCi"), UCICMD_UCI);
+        ASSERT_EQ(up.cmd, UCICMD_UCI);
+        ASSERT_EQ(up.parse("UCI"), UCICMD_UCI);
+        ASSERT_EQ(up.cmd, UCICMD_UCI);
+    }
+
+    TEST_F(AnUciParser, Return_UCI_CommandWhenParsesAStringEqualTo_uci_AlsoLeadingAndTrailingSpaces)
+    {
+        ASSERT_EQ(up.parse(" uci"), UCICMD_UCI);
+        ASSERT_EQ(up.cmd, UCICMD_UCI);
+        ASSERT_EQ(up.parse("uci "), UCICMD_UCI);
+        ASSERT_EQ(up.cmd, UCICMD_UCI);
+        ASSERT_EQ(up.parse("   uci  "), UCICMD_UCI);
+        ASSERT_EQ(up.cmd, UCICMD_UCI);
+        ASSERT_EQ(up.parse("\tuci"), UCICMD_UCI);
+        ASSERT_EQ(up.cmd, UCICMD_UCI);
+        ASSERT_EQ(up.parse("  \t  uci\t"), UCICMD_UCI);
+        ASSERT_EQ(up.cmd, UCICMD_UCI);
+    }
 
 }   // namespace UciParser
